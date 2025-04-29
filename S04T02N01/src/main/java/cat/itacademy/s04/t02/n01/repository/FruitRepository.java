d414e28121ac5a30dac1e4a3330a6dfd88a9aea3
Cinsara <cintia.csr18@gmail.com>
2025-04-29 17:20:32 +0200

    Upload 4.02 level 1 exercise

---
+++ S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/repository/FruitRepository.java
@@ -0,0 +1,10 @@
+package cat.itacademy.s04.t02.n01.repository;
+
+import cat.itacademy.s04.t02.n01.model.Fruit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FruitRepository extends JpaRepository<Fruit,Integer> {
+    List<Fruit> findByName(String name);
+}
